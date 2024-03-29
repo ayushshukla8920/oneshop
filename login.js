cfa0671291e2a242263f22d684c1cd5c7a32e77e
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2024-03-30 01:40:16 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "superstore"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.use(express.static('app'));
+
+app.get('/data', (req, res) => {
+  con.query('SELECT * FROM employee', (error, results, fields) => {
+    if (error) {
+      console.error('Error querying database:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+
