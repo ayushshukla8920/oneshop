42b62709d1ad02e72c1417e17ca6128c150ac1a8
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2024-03-30 15:52:40 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -40,8 +40,37 @@ app.get('/cust', (req, res) => {
   });
 });
 
+app.get('/submit-form', (req, res) => {
+  const custid = req.query.CustomerID;
+  const name = req.query.Name;
+  const phone = req.query.ContactNumber;
+  const email = req.query.Email;
+  const date = req.query.DateOfRegistration;
+
+  // Here you can process the form data as needed, such as inserting into a database
+  var query = "INSERT INTO customers VALUES ("+custid+",'"+name+"','"+phone+"','"+email+"','"+date+"')";
+  console.log(query);
+  con.query(query,(error, results) => {
+    if (error) {
+        console.error('Error inserting into MySQL: ' + error);
+        res.status(500).send('Internal Server Error');
+        return;
+    }
+    console.log('Inserted into MySQL:', results);
+    res.send(`
+            <script>
+                alert('Record inserted successfully!');
+                window.location.href = '/dashboard.html';
+            </script>
+        `);
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+
 
 
